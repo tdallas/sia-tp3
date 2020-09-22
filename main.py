90f6430a7b16629376a8de6a3393458ba932e130
tdallas <tdallas@itba.edu.ar>
2020-09-22 13:31:26 -0300

    added xor in main

--- main.py
+++ main.py
@@ -36,3 +36,22 @@ print(or_perceptron.guess([-1,1]))
 print(or_perceptron.guess([1,-1]))
 print(or_perceptron.guess([-1,-1]))
 print(or_perceptron.guess([1,1]))
+
+
+print('XOR: ')
+# OR input data
+xor_input_data = numpy.array([
+        [-1, 1],
+        [1, -1],
+        [-1, -1],
+        [1, 1]
+    ])
+# OR expected result for input
+xor_input_expected_data = numpy.array([1, 1, -1, -1])
+
+xor_perceptron = SimplePerceptron(or_input_data, or_input_expected_data)
+xor_perceptron.train()
+print(xor_perceptron.guess([-1,1]))
+print(xor_perceptron.guess([1,-1]))
+print(xor_perceptron.guess([-1,-1]))
+print(xor_perceptron.guess([1,1]))
--- simple_perceptron.py
+++ simple_perceptron.py
@@ -2,7 +2,7 @@ import numpy as numpy
 
 class SimplePerceptron():
     
-    def __init__(self, training_inputs, training_expected_values, eta=1, iterations=1000):
+    def __init__(self, training_inputs, training_expected_values, eta=0.025, iterations=10000):
         self.eta = eta
         self.iterations = iterations
         self.input_size = len(training_inputs[0])
