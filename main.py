45eb250500d16321e7ca64a0b2cd33d3f39136e8
tdallas <tdallas@itba.edu.ar>
2020-09-22 12:22:45 -0300

    or is not working

--- main.py
+++ main.py
@@ -20,20 +20,19 @@ print(perceptron.guess([1,-1]))
 print(perceptron.guess([1,1]))
 
 print('OR: ')
-
 # OR input data
-input_data = numpy.array([
+or_input_data = numpy.array([
         [-1, 1],
         [1, -1],
         [-1, -1],
         [1, 1]
     ])
 # OR expected result for input
-input_expected_data = numpy.array([1, 1, -1, -1])
+or_input_expected_data = numpy.array([1, 1, -1, -1])
 
-perceptron = SimplePerceptron(input_data, input_expected_data)
-perceptron.train()
-print(perceptron.guess([-1,1]))
-print(perceptron.guess([1,-1]))
-print(perceptron.guess([-1,-1]))
-print(perceptron.guess([1,1]))
+or_perceptron = SimplePerceptron(or_input_data, or_input_expected_data)
+or_perceptron.train()
+print(or_perceptron.guess([-1,1]))
+print(or_perceptron.guess([1,-1]))
+print(or_perceptron.guess([-1,-1]))
+print(or_perceptron.guess([1,1]))
