5c38ce6c7539a072c3aed373d652a0cd66d87b96
tdallas <tdallas@itba.edu.ar>
2020-09-23 19:42:28 -0300

    fix multilayer finally

--- ej_3/multilayer_perceptron.py
+++ ej_3/multilayer_perceptron.py
@@ -24,6 +24,8 @@ class MultilayerPerceptron():
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        # print('weights1', self.hidden_layers[0].synaptic_weights)
+        # print('weights2', self.output_layer.synaptic_weights)
         for _ in range(number_of_training_iterations):
             # Pass the training set through our neural network
             outputs = self.think(training_set_inputs)
@@ -37,29 +39,31 @@ class MultilayerPerceptron():
 
             # Calculate how much to adjust the weights by
             adjustments = self.get_adjustments(training_set_inputs, deltas, outputs)
-            print('adjustments',adjustments)
-            print('\noutput layer', self.output_layer.synaptic_weights)
-            print('\nhidden 0',self.hidden_layers[0].synaptic_weights)
-            print('\nhidden 1', self.hidden_layers[1].synaptic_weights)
+            # print('adjustments',adjustments)
+            # print('\nhidden 1', self.hidden_layers[1].synaptic_weights)
             
             self.adjust_weights(adjustments)
 
     def adjust_weights(self, adjustments):
-        indexAdjustments = len(adjustments) - 1
-        indexHiddenLayers = 0
-        while indexAdjustments > 0:
-            # print('pesos', self.hidden_layers[indexHiddenLayers].synaptic_weights)
-            # print('adjustments', adjustments[indexAdjustments])
-            self.hidden_layers[indexHiddenLayers].synaptic_weights += adjustments[indexAdjustments]
-            indexAdjustments-=1
-            indexHiddenLayers+=1
-        self.output_layer.synaptic_weights += adjustments[0] 
+        index = 0
+        while index < len(self.hidden_layers):
+            # print('index', index)
+            # print('pesos', self.hidden_layers[index].synaptic_weights)
+            # print('adjustment[index',adjustments[index])
+            self.hidden_layers[index].synaptic_weights += adjustments[index]
+            index+=1
 
     def get_adjustments(self, training_set_inputs, deltas, outputs):
         adjustments = []
-        for index in (len(deltas)-1, 1):
-            adjustments.append(outputs[index].T.dot(deltas[index]))
-        adjustments.append(training_set_inputs.T.dot(deltas[0]))    
+        adjustments.append(training_set_inputs.T.dot(deltas[-1]))
+        
+        outputs_index = len(outputs) - 2
+        deltas_index = len(deltas) - 2
+
+        while outputs_index >= 0:
+            adjustments.append(outputs[outputs_index].T.dot(deltas[deltas_index]))
+            outputs_index-=1
+            deltas_index-=1
         return adjustments
 
     def get_errors(self, training_expected_outputs, outputs):
@@ -79,13 +83,13 @@ class MultilayerPerceptron():
         previous_weights = self.output_layer.synaptic_weights.T
         current_hidden_layer = len(self.hidden_layers) - 1
         index = len(outputs) - 2
-        while index >= 1:
+        while index >= 0:
             previous_error = previous_delta.dot(previous_weights)
             errors.append(previous_error)
             previous_delta = previous_error * self.__sigmoid_derivative(outputs[index])
             deltas.append(previous_delta)
             
-            previous_weights = self.hidden_layers[current_hidden_layer]
+            previous_weights = self.hidden_layers[current_hidden_layer].synaptic_weights.T
             current_hidden_layer -= 1
 
             index -= 1
@@ -97,19 +101,24 @@ class MultilayerPerceptron():
         outputs = []
         input = inputs
         # input * first hidden layer
+        print(input)
         input = self.__sigmoid(dot(input, self.hidden_layers[0].synaptic_weights))
         outputs.append(input)
         
+        i = 1
         # product between hidden layers
-        for i in range(1, len(self.hidden_layers)):
+        while i < len(self.hidden_layers):
             input = self.__sigmoid(dot(input, self.hidden_layers[i].synaptic_weights))
             outputs.append(input)
-
+            i+=1
         # last product
+        # print('input:', input)
+        # print('output_layer', self.output_layer.synaptic_weights)
         outputs.append(self.__sigmoid(dot(input, self.output_layer.synaptic_weights)))
         
         # print(outputs)
         # outputs size = #hiddenlayers + 1 
+        # print('outputs',outputs)
         return outputs
 
     def print_weights(self):
