2e6c1673e6f35e0d1019e54d71687f7416babb90
tdallas <tdallas@itba.edu.ar>
2020-09-23 19:50:23 -0300

    still trying to fix stuffs

--- ej_3/main.py
+++ ej_3/main.py
@@ -8,13 +8,13 @@ random.seed(1)
 hidden_layer_1 = NeuronLayer(5, 2)
 
 # # Create layer 1 (5 neurons, each with 2 inputs)
-# hidden_layer_2 = NeuronLayer(5, 5)
+hidden_layer_2 = NeuronLayer(5, 5)
 
 # Create layer 2 (a single neuron with 4 inputs)
 layer2 = NeuronLayer(1, 5)
 
 # Combine the layers to create a neural network
-neural_network = MultilayerPerceptron([hidden_layer_1], layer2)
+neural_network = MultilayerPerceptron([hidden_layer_1, hidden_layer_2], layer2)
 
 # print("Stage 1) Random starting synaptic weights: ")
 neural_network.print_weights()
@@ -33,14 +33,23 @@ neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
 # Test the neural network with a new situation.
 # print("Stage 3) Considering a new situation [1, 1] -> ?: ")
-hidden_state, output = neural_network.think(array([0, 1]))
-print(output)
+print('TERMINO')
+output = neural_network.think(array([0, 1]))
+print('todo el output', output)
+print(output[-1])
+print('\n')
 
-hidden_state, output = neural_network.think(array([0, 0]))
-print(output)
+output = neural_network.think(array([0, 0]))
+print('todo el output', output)
+print(output[-1])
+print('\n')
 
-hidden_state, output = neural_network.think(array([1, 0]))
-print(output)
+output = neural_network.think(array([1, 0]))
+print('todo el output', output)
+print(output[-1])
+print('\n')
 
-hidden_state, output = neural_network.think(array([1, 1]))
-print(output)
+output = neural_network.think(array([1, 1]))
+print('todo el output', output)
+print(output[-1])
+print('\n')
