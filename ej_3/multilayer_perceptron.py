266519a848c255d38973556392a8ea740fe9c1ba
tdallas <tdallas@itba.edu.ar>
2020-09-23 15:10:08 -0300

    errors and deltas

--- ej_3/multilayer_perceptron.py
+++ ej_3/multilayer_perceptron.py
@@ -28,15 +28,12 @@ class MultilayerPerceptron():
             # Pass the training set through our neural network
             outputs = self.think(training_set_inputs)
 
-            # Calculate the error for layer 2 (The difference between the desired output
-            # and the predicted output).
-            layer2_error = training_set_outputs - output_from_layer_2
-            layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
+            # Calculate the error for all layers
+            # errors[0] is error from output layer
+            errors, deltas = self.get_errors(training_set_outputs, outputs)
 
-            # Calculate the error for layer 1 (By looking at the weights in layer 1,
-            # we can determine by how much layer 1 contributed to the error in layer 2).
-            layer1_error = layer2_delta.dot(self.output_layer.synaptic_weights.T)
-            layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
+            print('errors', errors)
+            print('deltas', deltas)
 
             # Calculate how much to adjust the weights by
             layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
@@ -46,7 +43,43 @@ class MultilayerPerceptron():
             self.hidden_layers[0].synaptic_weights += layer1_adjustment
             self.output_layer.synaptic_weights += layer2_adjustment
 
-    # The neural network thinks.
+#             # Calculate the error for layer 1 (By looking at the weights in layer 1,
+#             # we can determine by how much layer 1 contributed to the error in layer 2).
+#             layer1_error = layer2_delta.dot(self.output_layer.synaptic_weights.T)
+#             layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
+    def get_errors(self, training_expected_outputs, outputs):
+        errors = []
+        deltas = []
+
+        # from output
+        output_error = training_expected_outputs - outputs[-1]
+        errors.append(output_error)
+        output_delta = output_error * self.__sigmoid_derivative(outputs[-1])
+        deltas.append(output_delta)
+        
+        #hidden layers
+        previous_error = output_error
+        previous_delta = output_delta
+        print('pesos iniciales', self.output_layer.synaptic_weights.T)
+        previous_weights = self.output_layer.synaptic_weights.T
+        current_hidden_layer = len(self.hidden_layers) - 1
+        index = len(outputs) - 2
+        while index >= 1:
+            print('adentro', index, previous_delta, previous_weights)
+            previous_error = previous_delta.dot(previous_weights)
+            print(previous_error)
+            errors.append(previous_error)
+            previous_delta = previous_error * self.__sigmoid_derivative(outputs[index])
+            deltas.append(previous_delta)
+            
+            previous_weights = self.hidden_layers[current_hidden_layer]
+            current_hidden_layer -= 1
+
+            index -= 1
+            
+        return errors, deltas
+
+    # The neural network thinks
     def think(self, inputs):
         outputs = []
         input = inputs
@@ -62,13 +95,14 @@ class MultilayerPerceptron():
         # last product
         outputs.append(self.__sigmoid(dot(input, self.output_layer.synaptic_weights)))
         
-        print(outputs)
+        # print(outputs)
         # outputs size = #hiddenlayers + 1 
         return outputs
 
     def print_weights(self):
-        print("    Hidden layers\n")
-        for i in range(len(self.hidden_layers)):
-            print(self.hidden_layers[i].synaptic_weights)
-        print("\n    Output layer 2 (1 neuron, with 4 inputs):")
-        print(self.output_layer.synaptic_weights)
+        print('')
+        # print("    Hidden layers\n")
+        # for i in range(len(self.hidden_layers)):
+        #     print(self.hidden_layers[i].synaptic_weights)
+        # print("\n    Output layer 2 (1 neuron, with 4 inputs):")
+        # print(self.output_layer.synaptic_weights)
